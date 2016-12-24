0933dcc9df80ca946a38a65e023793ada5f96db0
chen <chenk@jfrog.com>
2016-12-24 14:51:07 +0200

    support for log rotators by http

--- log_rotator.go
+++ log_rotator.go
@@ -3,30 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/hpcloud/tail"
-	"time"
 	"net/http"
 )
-
 
 func main() {
 
 	http.HandleFunc("/log", logData)
-	fmt.Println("serving on http://localhost:8000/hello")
-	http.ListenAndServe("localhost:8000", nil)
-
-	// used for testing
-	var quitServer = make(chan bool)
-	var quitIndexer = make(chan bool)
-	var quitPersist = make(chan bool)
-	var quitEvent = make(chan bool)
-	go TailLogs("[XRAY-SERVER] ","/Users/chenk/.xray/logs/xray_server.log",quitServer)
-	go TailLogs("[XRAY-INDEXER] ","/Users/chenk/.xray/logs/xray_indexer.log",quitIndexer)
-	go TailLogs("[XRAY-PERSIST] ","/Users/chenk/.xray/logs/xray_persist.log",quitPersist)
-	go TailLogs("[XRAY-EVENT] ","/Users/chenk/.xray/logs/xray_event.log",quitEvent)
-
-	time.Sleep(500 * time.Minute)
+	fmt.Println("serving on http://localhost:8000/log")
+	http.ListenAndServe(":8080", nil)
 }
-func TailLogs(logName,logPath string,quit chan bool) {
+func TailLogs(logName, logPath string, quit chan bool) {
 	t, _ := tail.TailFile(logPath, tail.Config{
 		Follow: true,
 		ReOpen: true})
@@ -34,7 +20,7 @@ func TailLogs(logName,logPath string,quit chan bool) {
 		fmt.Println(logName + line.Text)
 	}
 	select {
-	case  <-quit:
+	case <-quit:
 		return
 	default:
 		// do nothing
@@ -42,6 +28,14 @@ func TailLogs(logName,logPath string,quit chan bool) {
 }
 
 func logData(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Listen to log Rotator")
-}
 
+	// used for testing
+	var quitServer = make(chan bool)
+	var quitIndexer = make(chan bool)
+	var quitPersist = make(chan bool)
+	var quitEvent = make(chan bool)
+	go TailLogs("[XRAY-SERVER] ", "/Users/chenk/.xray/logs/xray_server.log", quitServer)
+	go TailLogs("[XRAY-INDEXER] ", "/Users/chenk/.xray/logs/xray_indexer.log", quitIndexer)
+	go TailLogs("[XRAY-PERSIST] ", "/Users/chenk/.xray/logs/xray_persist.log", quitPersist)
+	go TailLogs("[XRAY-EVENT] ", "/Users/chenk/.xray/logs/xray_event.log", quitEvent)
+}
